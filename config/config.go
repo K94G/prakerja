dcb65efeeb29dee5ffbbfa04ec9bef79c839fc31
Kukuh Gumilang <k94gilang@gmail.com>
2023-04-12 15:14:00 +0800

    change conn

--- config/config.go
+++ config/config.go
@@ -10,9 +10,9 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "root:kgxampp@tcp(127.0.0.1:3306)/emergency?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:kgxampp@tcp(127.0.0.1:3306)/emergency?charset=utf8mb4&parseTime=True&loc=Local"
 	// dsn := "root:mfTcjoxxcAZkOtPHIiFy@tcp(containers-us-west-198.railway.app:6659)/railway?charset=utf8mb4&parseTime=True&loc=Local"
-	// dsn := "root:aTeYT6l8CC8PM7oh2S1R@tcp(containers-us-west-10.railway.app:5680)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:aTeYT6l8CC8PM7oh2S1R@tcp(containers-us-west-10.railway.app:5680)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
