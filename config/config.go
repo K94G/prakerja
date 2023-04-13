b01599a1da8b7561a009350ed958be0f28ef80dd
Kukuh Gumilang <k94gilang@gmail.com>
2023-04-13 09:45:37 +0800

    solve create contact

--- config/config.go
+++ config/config.go
@@ -10,9 +10,15 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
+	// local
 	// dsn := "root:kgxampp@tcp(127.0.0.1:3306)/emergency?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// kgilang
 	// dsn := "root:mfTcjoxxcAZkOtPHIiFy@tcp(containers-us-west-198.railway.app:6659)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// k94g
 	dsn := "root:aTeYT6l8CC8PM7oh2S1R@tcp(containers-us-west-10.railway.app:5680)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -22,28 +28,15 @@ func InitDB() {
 }
 
 func migration() {
-	var user model.User
-	var category1 model.Category
-	var category2 model.Category
-
 	DB.AutoMigrate(&model.User{})
-	DB.First(&user, 1)
-	if user.Name == "" {
-		user1 := model.User{Id: 1, Name: "Kukuh Gumilang", Username: "kg", Password: "secretkey-kg"}
-		DB.Create(&user1)
-	}
+	createUser1 := model.User{Id: 1, Name: "Kukuh Gumilang", Username: "kg", Password: "secretkey-kg"}
+	DB.Save(&createUser1)
 
 	DB.AutoMigrate(&model.Category{})
-	DB.First(&category1, 1)
-	if category1.Name == "" {
-		category1 := model.Category{Id: 1, Name: "Pemadam Kebakaran"}
-		DB.Create(&category1)
-	}
-	DB.First(&category2, 2)
-	if category2.Name == "" {
-		category2 := model.Category{Id: 2, Name: "Ambulance"}
-		DB.Create(&category2)
-	}
+	createCategory1 := model.Category{Id: 1, Name: "Pemadam Kebakaran"}
+	DB.Save(&createCategory1)
+	createCategory2 := model.Category{Id: 2, Name: "Ambulance"}
+	DB.Save(&createCategory2)
 
 	DB.AutoMigrate(&model.Contact{})
 }
--- go.mod
+++ go.mod
@@ -4,14 +4,13 @@ go 1.20
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/labstack/echo/v4 v4.10.2
+	github.com/labstack/echo/v4 v4.1.17
 	gorm.io/driver/mysql v1.5.0
 	gorm.io/gorm v1.25.0
 )
 
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
@@ -23,5 +22,4 @@ require (
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
 )
